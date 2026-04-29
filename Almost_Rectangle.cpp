439dfdd7721e9fddec5d378000d7b4da3e067d11
3MORA1 <omarkaramallahcis1@gmail.com>
2026-04-29 16:28:41 +0300

    Add Almost_Rectangle.cpp for area calculation

---
+++ Almost_Rectangle.cpp
@@ -0,0 +1,24 @@
+
+#include<iomanip>
+#include<iostream>
+#include<cmath>                                  
+using namespace std;
+int main() {
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+	
+	cout << fixed << setprecision(6) << (0.5 * b * a) + (0.5 * b * c) + (0.5 * d * a) - (0.5 * d * c)<<endl;
+
+}
+
+
+
+			
+		
+		
+		
+		
+	
+	
+	
+	
